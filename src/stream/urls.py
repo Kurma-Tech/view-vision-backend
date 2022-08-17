c1abfaad3827e716518df71a59b3883ba62b669f
KarmaTsring <santosh177gt@gmail.com>
2022-08-17 15:38:09 +0545

    Stream (api.py) file restored in src.stream.

--- src/stream/urls.py
+++ src/stream/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from .views import StreamView
+from .api import StreamAPI
 
 urlpatterns = [
     path("", StreamView.as_view()),
-    path("<int:id>", StreamView.as_view())
-    # path("<int:id>", DeviceView.as_view()),
+    path("<int:id>", StreamView.as_view()),
+    path("streamAPI/<int:id>", StreamAPI.as_view())
 ]
